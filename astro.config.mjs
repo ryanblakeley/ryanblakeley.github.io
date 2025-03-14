ae5adc818e508a07f8fee2bd5e3e0365618f661d
Ryan Blakeley <ryan@ryanblakeley.net>
2025-03-14 09:40:56 -0600

    Remove contact form, blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
